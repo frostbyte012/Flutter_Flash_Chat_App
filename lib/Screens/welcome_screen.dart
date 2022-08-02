078ad22b6d7183a8b510e4583c6d9f620ceb3045
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-08-02 13:56:22 +0530

    fix : Update Input Decoration in welcome_screen.dart file

--- lib/Screens/welcome_screen.dart
+++ lib/Screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/Screens/login_screen.dart';
 import 'package:flutter_chat_app/Screens/registration_screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter_chat_app/Components/primary_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
 
@@ -12,6 +13,18 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+
+
+  void navigationLogin()
+  {
+    Navigator.pushNamed(context, LoginScreen.id);
+  }
+
+  void navigationRegistration()
+  {
+    Navigator.pushNamed(context, RegistrationScreen.id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,50 +58,20 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                     onTap: () {
                       print("Tap Event");
                     },
+                    totalRepeatCount: 100,
                   ),
                 ),
               ],
             ),
+
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                    Navigator.pushNamed(context,RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+
+            PrimaryButton(fun: navigationLogin, colour: Colors.lightBlueAccent, text: 'Log in'),
+
+            PrimaryButton(fun: navigationRegistration, colour: Colors.blueAccent, text: 'Register'),
+
           ],
         ),
       ),
