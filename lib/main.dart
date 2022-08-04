59a70af8147c8422cb046dbab1341b04a8f3f8f5
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-08-04 22:13:00 +0530

    build : Add main.dart file for the execution and also adding dynamic async future type in the main func

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'Screens/welcome_screen.dart';
 import 'Screens/login_screen.dart';
 import 'Screens/registration_screen.dart';
 import 'Screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(FlashChat());
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
